b6156c2ffbf9a6ec8b5281e50070bc1c7fd35570
ElderMayday <galdarsar@gmail.com>
2018-03-14 21:41:35 +0100

    Begin coding the entry class

--- solver/src/main/java/general/Main.java
+++ solver/src/main/java/general/Main.java
@@ -1,5 +1,10 @@
 package general;
 
+import solving.selectors.Selector;
+import solving.selectors.SelectorDorigo;
+import solving.selectors.SelectorManiezzo;
+import solving.selectors.SelectorStandard;
+
 import java.util.Random;
 
 public class Main
@@ -9,6 +14,53 @@ public class Main
 
     public static void main(String[] args)
     {
-        System.out.println("Finished");
+        if (args.length < 4)
+        {
+            System.out.println("EXCEPTION: Not all arguments are specified:\njava -jar solver.jar [configuration id] " +
+                    "[instance id] [seed] [instance path] {rest of the arguments}");
+
+            return;
+        }
+
+        random = new Random(Integer.parseInt(args[2]));
+
+        int selectorID = 1;
+        Selector selector = null;
+        double alpha = 1.0, beta = 1.0;
+        double probabilityDorigo = 0.5;
+
+        for (int i = 4; i < args.length;)
+        {
+            if (args[i].equals("--selector-standard"))
+            {
+                selectorID = 1;
+                i++;
+            }
+
+            if (args[i].equals("--selector-dorigo"))
+            {
+                selectorID = 2;
+                i++;
+            }
+
+            if (args[i].equals("--selector-maniezzo"))
+            {
+                selectorID = 3;
+                i++;
+            }
+        }
+
+        switch (selectorID)
+        {
+            case 1:
+                selector = new SelectorStandard(alpha, beta);
+                break;
+            case 2:
+                selector = new SelectorDorigo(alpha, beta, probabilityDorigo);
+                break;
+            case 3:
+                selector = new SelectorManiezzo(alpha);
+                break;
+        }
     }
 }
