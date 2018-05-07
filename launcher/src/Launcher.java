2069a27d064014349732ca028379a101fa36787c
ElderMayday <galdarsar@gmail.com>
2018-05-07 03:38:59 +0200

    Make preliminary version

--- launcher/src/Launcher.java
+++ launcher/src/Launcher.java
@@ -8,16 +8,30 @@ public class Launcher
     public static void main(String[] args)
     {
         String configurationString = "testConfig2 instance1 1234567 [path]  " +
-                "--selector-dorigo --alpha 0.2 --beta 3.56 --dorigo-probability 0.72 --candidate-no --elitist-ant-s " +
-                "--local-update-yes --iterated-ants --ant-num 6 --lupd-epsilon 0.46 --tau0 0.19 --local-search-twohalf " +
-                "--evaporation-remains 0.66 --eas-m-elite 90 --eas-bounded-yes --eas-k 4.02 --reinitialization-yes " +
-                "--reinitialization-time 0.13 --iterated-criteria-best --destruction-probability 0.15 --end";
+                "--selector-standard --alpha 5.0 --beta 5.0 --candidate-yes --candidate-ratio 0.25 --ant-s " +
+                "--local-update-no --standard --ant-num 30 --local-search-twohalf --evaporation-remains 0.75 " +
+                "--ant-s-bounded-no --reinitialization-no --end";
 
         String[] arguments = configurationString.split("\\s+");
 
         String path = "D:\\master\\irace\\test\\";
 
-        String[] names = {"Golden_1.vrp.txt",
+        String[] names = {"CMT1.vrp.txt",
+                "CMT2.vrp.txt",
+                "CMT3.vrp.txt",
+                "CMT4.vrp.txt",
+                "CMT5.vrp.txt",
+                "CMT6.vrp.txt",
+                "CMT7.vrp.txt",
+                "CMT8.vrp.txt",
+                "CMT9.vrp.txt",
+                "CMT10.vrp.txt",
+                "CMT11.vrp.txt",
+                "CMT12.vrp.txt",
+                "CMT13.vrp.txt",
+                "CMT14.vrp.txt"};
+
+        /*String[] names = {"Golden_1.vrp.txt",
                 "Golden_2.vrp.txt",
                 "Golden_3.vrp.txt",
                 "Golden_4.vrp.txt",
@@ -36,7 +50,7 @@ public class Launcher
                 "Golden_17.vrp.txt",
                 "Golden_18.vrp.txt",
                 "Golden_19.vrp.txt",
-                "Golden_20.vrp.txt"};
+                "Golden_20.vrp.txt"};*/
 
         /*String[] names = {"X-n101-k25.vrp.txt",
                 "X-n125-k30.vrp.txt",
